563c63101d2863e07b9c3151108a9013a2f547b3
kb <wirechamp@gmail.com>
2017-08-27 13:21:40 -0500

    Added configurations for TypeScript and Webpack

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: ['./src/app'],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts','.tsx']
+    },
+    module: {
+        loaders: [{
+            test: /\.tsx?$/,
+            exclude: /node_modules/,
+            loaders: ['ts-loader']
+        }]
+    }
+};
